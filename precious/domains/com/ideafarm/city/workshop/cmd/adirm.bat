3a4dda01fdc65892c8e3a989227495d905c4d8a8
Wo'O Ideafarm <wo.ideafarm.publication.delayed.4.yrs@ideafarm.com>
2024-03-22 14:17:35 -0700

    adirm.bat: glass

--- precious/domains/com/ideafarm/city/workshop/cmd/adirm.bat
+++ precious/domains/com/ideafarm/city/workshop/cmd/adirm.bat
@@ -5,15 +5,25 @@ if %1z == z goto :NEEDtIMESTAMP
 if not %2z == z goto :HAVEmOUNT
 
 d:
-cd \ideafarm.home.101\devices
+if not exist \tmp\* md \tmp
+cd               \tmp
+if not %cd% == D:\tmp goto :FIN
+md               adirm.%1
+cd               \tmp\adirm.%1
+if not %cd% == D:\tmp\adirm.%1 goto :FIN
+
+cd               \ideafarm.home.101\devices
+if not %cd% == D:\ideafarm.home.101\devices goto :FIN
+
 for /d %%d in (*) do @if exist %%d\* start "%%d" adirm.bat %1 %%d
+
 goto :FIN
 
 
 :HAVEmOUNT
 d:
 cd \ideafarm.home.101\devices\%2\ideafarm.archives\wo.ideafarm
-for /d %%y in (2019 2021 2022 2023 2099) do @dir /s /o:-d %%y > d:\tmp\adirm.%1.%2.%%y.ttt
+for /d %%y in (2019 2021 2022 2023 2099) do @dir /s /o:-d %%y > d:\tmp\adirm.%1\adirm.%1.%2.%%y.ttt
 rem for /d %%y in (*) do @dir /s /o:-d %%y > d:\tmp\adirm.%1.%2.%%y.ttt
 exit
 
