2b7e763fdd5351c4f81dab107c4c1f69604ba08e
ideafarm <wo.ideafarm.publication.delayed.4.yrs@ideafarm.com>
2024-06-29 18:51:30 -0700

    CIP chunk

--- precious/domains/com/ideafarm/city/workshop/cmd/t.bat
+++ precious/domains/com/ideafarm/city/workshop/cmd/t.bat
@@ -1,6 +1,5 @@
 @echo off
 
-d:
-cd D:\ideafarm.home.101\devices
-
-for /d %%d in (*) do @if exist %%d\ideafarm.archives\wo.ideafarm\2099\*.chunk.* echo %%d
+fc e:%1 f:%1 > nul
+if not %errorlevel% == 0 echo different: %1
+if     %errorlevel% == 0 echo same     : %1
