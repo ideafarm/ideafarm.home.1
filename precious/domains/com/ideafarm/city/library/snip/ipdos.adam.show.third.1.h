a16a2fc395683bcc0b844ebee76a47db8d472031
ideafarm <wo.ideafarm.publication.delayed.4.yrs@ideafarm.com>
2025-02-22 12:43:55 -0800

    header refresh ; polish dns

--- precious/domains/com/ideafarm/city/library/snip/ipdos.adam.show.third.1.h
+++ precious/domains/com/ideafarm/city/library/snip/ipdos.adam.show.third.1.h
@@ -12126,6 +12126,49 @@ these values are used in the foreign domain name system
 */
 /**/
 
+//    
+//    Source: RFC 1035, p. 11:
+//    
+//    TYPE            value and meaning
+//    
+//    A               1 a host address
+//    
+//    NS              2 an authoritative name server
+//    
+//    MD              3 a mail destination (Obsolete - use MX)
+//    
+//    MF              4 a mail forwarder (Obsolete - use MX)
+//    
+//    CNAME           5 the canonical name for an alias
+//    
+//    SOA             6 marks the start of a zone of authority
+//    
+//    MB              7 a mailbox domain name (EXPERIMENTAL)
+//    
+//    MG              8 a mail group member (EXPERIMENTAL)
+//    
+//    MR              9 a mail rename domain name (EXPERIMENTAL)
+//    
+//    NULL            10 a null RR (EXPERIMENTAL)
+//    
+//    WKS             11 a well known service description
+//    
+//    PTR             12 a domain name pointer
+//    
+//    HINFO           13 host information
+//    
+//    MINFO           14 mailbox or mail list information
+//    
+//    MX              15 mail exchange
+//    
+//    TXT             16 text strings
+//    
+//    
+//    Source: https://www.iana.org/assignments/dns-parameters/dns-parameters.xhtml#dns-parameters-4
+//    
+//    CAA 	257 	Certification Authority Restriction 	[RFC8659]
+//    
+
 /*1*//*#define ifcIDtYPEdNSqUERY*//*1*/
 
 /*3*/
