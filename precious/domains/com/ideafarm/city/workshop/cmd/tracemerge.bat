7000330397be9c81abc692c8266e420d1b58339f
Wo'O Ideafarm <wo.ideafarm.publication.delayed.4.yrs@ideafarm.com>
2025-06-23 08:55:18 -0700

    Update tracemerge.bat

--- precious/domains/com/ideafarm/city/workshop/cmd/tracemerge.bat
+++ precious/domains/com/ideafarm/city/workshop/cmd/tracemerge.bat
@@ -3,6 +3,6 @@
 d:
 cd \tmp
 for %%s in (wh1 wh2 dns1 go1) do copy \\%%s\d\ideafarm.home.101\ephemeral\ipdos\memorySpaces\Administrator\00000100\stateSpaces\fffeffff\bkstrace.* bkstrace.all.%%s.ttt
-dir bkstrace.all.*
 copy bkstrace.all.* bkstrace.all.all.ttt
 sort < bkstrace.all.all.ttt /o bkstrace.all.sorted.ttt
+dir bkstrace.all.*
