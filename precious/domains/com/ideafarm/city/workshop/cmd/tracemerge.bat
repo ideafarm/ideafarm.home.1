9578a1ebcac9de91a1ad1734e8fb645ae6543b88
ideafarm <wo.ideafarm.publication.delayed.4.yrs@ideafarm.com>
2025-08-03 11:38:05 -0700

    Update tracemerge.bat

--- precious/domains/com/ideafarm/city/workshop/cmd/tracemerge.bat
+++ precious/domains/com/ideafarm/city/workshop/cmd/tracemerge.bat
@@ -1,5 +1,7 @@
 @echo off
 
+rem param 1 (optional): path for output file
+
 adam 51000622 !idHome 10000008 !make ba8
 adam 510008ab !idHome 10000008
 
@@ -9,7 +11,9 @@ cd \ideafarm.home.1\tmp
 if not exist tracecopy.all.ttt echo file tracecopy.dns1.ttt not found
 if not exist tracecopy.all.ttt goto :FIN
 
-sort tracecopy.all.ttt /o tracecopy.all.sorted.ttt
+
+if     %1z == z sort tracecopy.all.ttt /o tracecopy.all.sorted.ttt
+if not %1z == z sort tracecopy.all.ttt /o \1\tracecopy.all.sorted.ttt
 dir tracecopy.*
 
 :FIN
