61b8f90f45f8bc018946c169f4ac0f7b4d6de9b9
ideafarm <wo.ideafarm.publication.delayed.4.yrs@ideafarm.com>
2025-08-03 12:16:36 -0700

    tracemerge now uses drive c ; trace book files bigger

--- precious/domains/com/ideafarm/city/workshop/cmd/tracemerge.bat
+++ precious/domains/com/ideafarm/city/workshop/cmd/tracemerge.bat
@@ -5,8 +5,8 @@ rem param 1 (optional): path for output file
 adam 51000622 !idHome 10000008 !make ba8
 adam 510008ab !idHome 10000008
 
-d:
-cd \ideafarm.home.1\tmp
+c:
+cd \tmp
 
 if not exist tracecopy.all.ttt echo file tracecopy.dns1.ttt not found
 if not exist tracecopy.all.ttt goto :FIN
