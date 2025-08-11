494ff3575e103ca4f8e9e7d6413dda142c3a984d
ideafarm <wo.ideafarm.publication.delayed.4.yrs@ideafarm.com>
2025-08-10 17:31:27 -0700

    oops

--- precious/domains/com/ideafarm/city/workshop/cmd/tmark.bat
+++ precious/domains/com/ideafarm/city/workshop/cmd/tmark.bat
@@ -5,4 +5,4 @@ rem param (optional): !end
 rem params may be specified in any order
 rem if !unique [string] is not specified then the string "0" will be used
 
-call a.bat TOOLtRACEmARK %1 %2
+call a.bat TOOLtRACEmARK %1 %2 %3 %4 %5 %6 %7 %8 %9
