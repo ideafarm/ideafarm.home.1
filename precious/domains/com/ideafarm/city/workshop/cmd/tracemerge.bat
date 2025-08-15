cf09a26e31a17aced62114186536a1ce33419074
ideafarm <wo.ideafarm.publication.delayed.4.yrs@ideafarm.com>
2025-08-15 13:29:26 -0700

    TOOLtRACEmARK / !m !e

--- precious/domains/com/ideafarm/city/workshop/cmd/tracemerge.bat
+++ precious/domains/com/ideafarm/city/workshop/cmd/tracemerge.bat
@@ -10,8 +10,10 @@ cd \tmp
 if not exist tracecopy.all.ttt echo file tracecopy.dns1.ttt not found
 if not exist tracecopy.all.ttt goto :FIN
 
+echo sorting
 if     %1z == z sort tracecopy.all.ttt /o tracecopy.all.sorted.ttt
 if not %1z == z sort tracecopy.all.ttt /o %1\tracecopy.all.sorted.ttt
+echo sorted
 
 if     %1z == z call a.bat TOOLfILEsPLIT !file ///c/tmp/tracecopy.all.sorted.ttt
 if not %1z == z call a.bat TOOLfILEsPLIT !file %1\tracecopy.all.sorted.ttt
