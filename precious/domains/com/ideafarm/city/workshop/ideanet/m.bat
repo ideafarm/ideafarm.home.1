16b74b5c15b3bbcbec9df9f0dc05d0bfe647bec7
Wo'O Ideafarm <wo.ideafarm.publication.delayed.4.yrs@ideafarm.com>
2025-11-08 08:24:36 -0800

    osMakeF / makes lib file

--- precious/domains/com/ideafarm/city/workshop/ideanet/m.bat
+++ precious/domains/com/ideafarm/city/workshop/ideanet/m.bat
@@ -1,4 +1,4 @@
 @echo off
 wpp386 -d2 -dBiFCcLASS=0 ideanet.cpp
-wlink.exe system nt op q op map=ideanet.map op mangle debug all name ideanet.exe file *.obj library D:\ideafarm.home.1\ephemeral\city\park\exedll\1\master\ideafarm.81000001.ipdos-wl op map=d:\tmp\ideanet.map
+wlink.exe system nt op q op map=ideanet.map op mangle debug all name ideanet.exe file *.obj library D:\ideafarm.home.1\ephemeral\city\park\exedll\1\master\ideafarm.510008bd.ipdos-wl library D:\ideafarm.home.1\ephemeral\city\park\exedll\1\master\ideafarm.81000001.ipdos-wl op map=d:\tmp\ideanet.map
                                                                                                     
