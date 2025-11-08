be248d78f79b2cf701d6585c6a905d1dc5e762a3
Wo'O Ideafarm <wo.ideafarm.publication.delayed.4.yrs@ideafarm.com>
2025-11-08 09:23:10 -0800

    before renaming 2source

--- precious/domains/com/ideafarm/city/workshop/ideanet/m.bat
+++ precious/domains/com/ideafarm/city/workshop/ideanet/m.bat
@@ -1,4 +1,11 @@
 @echo off
+
+cd \ideafarm.home.1\precious\domains\com\ideafarm\city\workshop\ideanet
+if exist *.obj del *.obj
+
+copy \ideafarm.home.1\ephemeral\city\workshop\3object\2*
+copy \ideafarm.home.1\ephemeral\city\workshop\3object\3*
+
 wpp386 -d2 -dBiFCcLASS=0 ideanet.cpp
-wlink.exe system nt op q op map=ideanet.map op mangle debug all name ideanet.exe file *.obj library D:\ideafarm.home.1\ephemeral\city\park\exedll\1\master\ideafarm.510008bd.ipdos-wl library D:\ideafarm.home.1\ephemeral\city\park\exedll\1\master\ideafarm.81000001.ipdos-wl op map=d:\tmp\ideanet.map
+wlink.exe system nt op q op map=ideanet.map op mangle debug all name ideanet.exe file *.obj library D:\ideafarm.home.1\ephemeral\city\park\exedll\1\master\ideafarm.81000001.ipdos-wl op map=d:\tmp\ideanet.map
                                                                                                     
