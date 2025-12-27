f65988348019cad03952ce25c350d14ce57c3add
Wo'O Ideafarm <wo.ideafarm.publication.delayed.4.yrs@ideafarm.com>
2025-12-26 21:13:31 -0800

    TOOLwALKsEARCH / debug and polish

--- precious/domains/com/ideafarm/city/workshop/cmd/myfind.bat
+++ precious/domains/com/ideafarm/city/workshop/cmd/myfind.bat
@@ -1,5 +1,5 @@
 @echo off
 
-rem EXAMPLE ARGUMENTS: !root ///ideafarm.home.1/ !find "save space"
+rem EXAMPLE ARGUMENTS: !root ///d/ideafarm.home.1/ !find "save space"
 
 call a.bat TOOLwALKsEARCH %1 %2 %3 %4 %5 %6 %7 %8 %9
