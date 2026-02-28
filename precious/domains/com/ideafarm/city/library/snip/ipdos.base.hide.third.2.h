df279fa7be16c77e45e24cebf8b5b5b01c657fa3
Wo'O Ideafarm <wo.ideafarm.publication.delayed.4.yrs@ideafarm.com>
2026-02-28 12:07:23 -0800

    epoch -> 21_9 seconds

--- precious/domains/com/ideafarm/city/library/snip/ipdos.base.hide.third.2.h
+++ precious/domains/com/ideafarm/city/library/snip/ipdos.base.hide.third.2.h
@@ -4814,7 +4814,7 @@ examples
 /*1*//*BITSsHIFTiDEAnETePOCH*//*1*/
 
 //#define BITSsHIFTiDEAnETePOCH ( 0x16 )
-#define BITSsHIFTiDEAnETePOCH ( 0x15 )
+#define BITSsHIFTiDEAnETePOCH ( 0x14 )
 
 
 //
